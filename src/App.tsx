b579737f06560e6fae3cc65470b61baf2e6b6047
Șindrilaru,Andreea A.D <a.sindrilaru@student.fontys.nl>
2023-06-14 19:46:40 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import NotFoundPage from './pages/NotFound/NotFoundPage';
 import RegisterPage from './pages/RegisterPage';
 import Navbar from './components/UserSide/Navigation/navbar';
 import LoginPage from './pages/loginPage';
-import Home from './pages/HomePage';
+import Home from './pages/homePage';
 function App() {
     // const [userRole, setUserRole] = useState('');
 
